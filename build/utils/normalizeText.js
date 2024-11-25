899433c5e5b4df52c3a4f8e622d522e09b7ceed7
Thiago Roncete <127981887+thiagoroncete@users.noreply.github.com>
2024-11-25 16:10:22 -0300

    Delete build directory

--- build/utils/normalizeText.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.normalizarTexto = normalizarTexto;
-function normalizarTexto(texto) {
-    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-}
-//# sourceMappingURL=normalizeText.js.map
